2a9fafe122a5a5d4d4a558c5358acdcfabb19880
AbdullahAsif <abdullah.asif6066@gmail.com>
2018-06-21 15:14:38 +0500

    3:14 update

---
+++ index.php
@@ -0,0 +1,57 @@
+<?php
+
+
+
+
+
+?>
+<!DOCTYPE html>
+<html>
+<script type="text/javascript">
+	
+</script>
+<style type="text/css">
+#float{
+
+	float: left;
+}
+#text_dec{
+	text-decoration: none ;
+
+}
+
+
+
+</style>
+<head>
+	<title>Home</title>
+</head>
+<body style="margin: .0em;font-family: calibri" >
+<div>
+	<div style="background-color: gray">
+		<span style="font-size: 30px;margin-left: 20px;">Exam Generator</span>
+		<span style="align-items: right;align-self: right;margin-left: 1500px ;margin-top: -5px">
+				<input style="margin-top: -10px;align-items: right;align-self: right;border-radius: 5px;background-color: yellow" type="button" name="" value="Admin sign in">
+		</span>
+		<br>
+	</div>
+</div>
+<div style="margin-top: 150px">
+	<div id="float" style="margin-left: 500px">
+		<a id="text_dec" href="sign_in.php">
+		<img src="student.png">
+		<br>
+		<span style="margin-left: 100px">Student</span>
+		</a>
+	</div>
+	<div >
+		<a id="text_dec" href="sign_in.php">
+		<img style="margin-left: 300px" src="teacher.png">
+		<br>
+		<span style="margin-left: 400px">Teacher</span>
+	    </a>
+	</div>
+</div>
+
+</body>
+</html>
